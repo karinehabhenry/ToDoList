a3d61759a5b3d1ce083195d4098b01c3e60e3e60
karinehabhenry <161515319+karinehabhenry@users.noreply.github.com>
2024-07-30 12:41:04 +0300

    Add project files.

---
+++ ToDoList/Controllers/ToDoController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoController : ControllerBase
+    {
+        private static List<ToDo>Todos = new List<ToDo>();
+
+        private static int Id = 1;
+
+
+        [HttpGet("GetTasks")]
+        public IActionResult GetAllTasks()
+        {
+            return Ok(Todos);
+        }
+
+
+        [HttpGet("GetById")]
+        public IActionResult GetTask(int id)
+        {
+            var ToDoID = Todos.SingleOrDefault(x => x.Id == id);
+            if (ToDoID == null)
+            {
+                return NotFound();
+            }
+            return Ok(ToDoID);
+        }
+
+        [HttpPost("PostTask")]
+        public IActionResult CreateTask(ToDo newTask)
+        {
+            newTask.Id = Id++;
+            Todos.Add(newTask);
+            return CreatedAtAction(nameof(GetTask), new { Id = newTask }, newTask);
+        }
+
+
+
+        [HttpDelete("DeleteTask")]
+        public IActionResult DeleteTask(int id)
+        {
+            var Task = Todos.SingleOrDefault(e => e.Id == id);
+            if (Task == null)
+            {
+                return NotFound();
+            }
+            Todos.Remove(Task);
+            return NoContent();
+        }
+
+
+
+        [HttpPut("PutTask")]
+        public IActionResult UpdateTask(ToDo updatedTask)
+        {
+            var Task = Todos.SingleOrDefault(e => e.Id == updatedTask.Id);
+
+            if (Task == null)
+            {
+                return NotFound();
+            }
+
+            Task.Name= updatedTask.Name;
+            Task.Description= updatedTask.Description;
+            Task.Deadline= updatedTask.Deadline;
+            Task.Duration= updatedTask.Duration;    
+
+            return Ok(updatedTask);
+
+
+        }
+
+    }
+}
---
+++ ToDoList/ToDo.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoList
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public int Duration { get; set; }
+    }
+}
